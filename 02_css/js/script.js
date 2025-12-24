ac2631a6cc35484ab5a305385f8f289218d6caa3
wish02sw-max <wish02sw@gmail.com>
2025-12-24 16:19:36 +0900

    Add files via upload

---
+++ 02_css/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+$("h1").css({"color":"red"})
+$(".date").css({"color":"#fff","background":"purple","font-size":80})
+$(".para").css({"border":"10px solid blue"})
+$("li").css({"border":"1px solid red"})
+$("ul li:last-child").css({"font-size":"50px"})
+$("a").css({"border":"5px solid blue","padding":"15px 10px"})
+})
