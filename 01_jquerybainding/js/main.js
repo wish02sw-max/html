af20af4263b0b6d342c89912f8a00a759a380388
wish02sw-max <wish02sw@gmail.com>
2025-12-24 15:34:45 +0900

    Add files via upload

---
+++ 01_jquerybainding/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+// 코드 작성
+// 버튼을 click 이벤트 하면 div박스의 내부 색상을 red로 변경하는 프로그램
+
+// 버튼을 클릭하면
+$('button').click(function(){
+    // div 박스의 내부색을 레드로 변경
+    $("div").css({"background":"red"})
+})
+
+})
